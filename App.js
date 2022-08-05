565f227beecba5e4c70733185ee0ee55465e4ab9
giovanafurlan <giovananelofurlan@gmail.com>
2022-08-04 22:00:37 -0300

    first commit

--- App.js
+++ App.js
@@ -5,46 +5,81 @@ import {
   TextInput,
   View 
 } from 'react-native';
+import { AiOutlineSearch } from "react-icons/ai";
+import { SearchBar } from 'react-native-elements';
 import { useState } from 'react';
 
 export default function App() {
 
-  const[ususario, setUsuario] = useState();
-  const[result, setResult] = useState();
+  const[usuario, onChangeUsuario] = useState();
+  const[avatar, setAvatar] = useState();
+  const[id, setId] = useState();
+  const[nome, setNome] = useState();
+  const[seguindo, setSeguindo] = useState();
+  const[seguidores, setSeguidores] = useState();
+  const[criacao, setCriacao] = useState();
 
   function busca(){
-    fetch('https://api.github.com/users/')
+    fetch('https://api.github.com/users/' + usuario)
     .then(res => {
       return res.json();
     })
     .then(data => {
-      const foto = data.message;
-      console.log(foto);
-      setResult(foto);
+      console.log(data);
+      const avatar = (data.avatar_url);
+      setAvatar(avatar);
+
+      const id = (data.id);
+      setId(id);
+
+      const nome = (data.name);
+      setNome(nome);
+
+      const following = (data.following);
+      setSeguindo(following);
+
+      const followers = (data.followers);
+      setSeguidores(followers);
+
+      const criacao = (data.created_at);
+      setCriacao(criacao);
     });
   };
 
   return (
     <View 
      style={styles.container}>
-      <TextInput />
-      <Pressable  
-        onPress={busca}
-        style={styles.btn}>
-          <Text style={{
+      <Text>Digite o username do git:</Text>
+      <View style={{
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center'
+      }}>
+        <TextInput 
+          value={usuario}
+          onChangeText={onChangeUsuario}
+          style={styles.input}/>
+        <Pressable  
+          onPress={busca}
+          style={styles.btn}>
+          <AiOutlineSearch style={{
             color: 'white'
-          }}>
-            Buscar
-          </Text>
+          }}/>
         </Pressable>
+      </View>
       <img 
-        src={result}
+        src={avatar}
         width="200"
         height="200"
         style={{
           border: '10px',
-          margin: '50px'
+          margin: '20px'
         }}/>
+        <Text>Id: {id}</Text>
+        <Text>Nome: {nome}</Text>
+        <Text>Followers: {seguidores}</Text>
+        <Text>Following: {seguindo}</Text>
+        <Text>Criado em: {criacao}</Text>
    </View>
   );
 }
@@ -59,10 +94,16 @@ const styles = StyleSheet.create({
   btn:{
     alignItems: 'center',
     justifyContent: 'center',
-    paddingVertical: 12,
-    paddingHorizontal: 32,
     borderRadius: 4,
-    elevation: 3,
-    backgroundColor: 'purple'
-  }
+    backgroundColor: 'purple',
+    width: 22,
+    height: 22,
+    fontSize: 15
+  },
+  input: {
+    height: 20,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
 });
